4622a53abebd49489918d971407ab55038a9746a
dqthuan <72246634+dqthuan@users.noreply.github.com>
2020-11-05 13:37:41 +0700

    Add files via upload

---
+++ test/test.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>tinh toan</title>
+</head>
+<body>
+<form method="post">
+   Nhap So : <input type="number" id="number" value="number">
+    <button onclick="myFunction">Calculate</button>
+</form>
+<script>
+    function myFunction() {
+        document.getElementById('number').innerText=value;
+        let a =;
+        for (let i = 0; i < 100; i++) {
+            if (a + i <) {
+                a += i;
+            }
+        }
+        document.write(a);
+    }
+</script>
+</body>
+</html>
